78af60cdbf763e47282e5c96b7db9ec834c4d0ad
AlessandroCampo <alessandrocampo97@gmail.com>
2024-01-09 14:31:42 +0100

    forgot about the console.logs

--- main.js
+++ main.js
@@ -8,21 +8,25 @@ for (i = 1; i <= 100; i++) {
     if (i % 3 == 0) {
         cell.innerText = "Fizz"
         cell.classList.add("cell-green")
+        console.log("Fizz")
     }
 
     else if (i % 5 == 0) {
         cell.innerText = "Buzz"
         cell.classList.add("cell-yellow")
+        console.log("Buzz")
     }
 
     else {
         cell.innerText = i
+        console.log(i)
         cell.classList.add("cell-blue")
     }
 
     if (i % 5 == 0 && i % 3 == 0) {
         cell.innerText = "FizzBuzz"
         cell.classList.add("cell-violet")
+        console.log("FizzBuzz")
     }
 
 
